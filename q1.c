0b820b767212ad55729e1e3b9ea86391bc7f1856
Son Hoang <son.t.hoang@tcu.edu>
2025-12-01 19:12:17 -0600

    q1 added

---
+++ q1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+float toCelsius(float f) {
+    return (f - 32.0f) * 5.0f / 9.0f;
+}
+
+int main(void) {
+    float f, c;
+
+    printf("Enter temperature in Fahrenheit: ");
+    scanf("%f", &f);
+
+    c = toCelsius(f);
+
+    printf("Temperature in Celsius: %.2f\n", c);
+
+    return 0;
+}
+

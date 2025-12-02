0cc5b10561e494324579f82f5bc7dd0851393871
Son Hoang <son.t.hoang@tcu.edu>
2025-12-01 19:22:42 -0600

    q2 added

---
+++ q2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int max3(int a, int b, int c) {
+    int max = a;
+    if (b > max)
+        max = b;
+    if (c > max)
+        max = c;
+    return max;
+}
+
+int main(void) {
+    int a, b, c;
+
+    printf("Enter three integers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    printf("The largest is: %d\n", max3(a, b, c));
+
+    return 0;
+}
+

efdf565aa51808ffd402e56850007aca4d5b27b4
Son Hoang <son.t.hoang@tcu.edu>
2025-12-01 21:55:00 -0600

    q19 add

---
+++ q19.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+struct Date {
+    int day;
+    int month;
+    int year;
+};
+
+struct Student {
+    char name[40];
+    int  roll;
+    float marks;
+    struct Date dob;   // date of birth
+};
+
+int main(void) {
+    struct Student s;
+
+    printf("Enter student name (no spaces): ");
+    scanf("%39s", s.name);
+
+    printf("Enter roll number: ");
+    scanf("%d", &s.roll);
+
+    printf("Enter marks: ");
+    scanf("%f", &s.marks);
+
+    printf("Enter date of birth (day month year): ");
+    scanf("%d %d %d", &s.dob.day, &s.dob.month, &s.dob.year);
+
+    printf("\nStudent Information\n");
+    printf("-------------------\n");
+    printf("Name : %s\n", s.name);
+    printf("Roll : %d\n", s.roll);
+    printf("Marks: %.2f\n", s.marks);
+    printf("DOB  : %02d-%02d-%04d\n", s.dob.day, s.dob.month, s.dob.year);
+
+    return 0;
+}
+
